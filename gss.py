80ae763be4e2375868c2172c9ff9320219a5d207
Alex Cook <alex@alexcook.com.au>
2020-08-17 13:13:55 +0930

    Rename Block class to GamePiece, blockers are now also a GamePiece

--- gss.py
+++ gss.py
@@ -5,41 +5,33 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-boardRowLabels = list('ABCDEF')
-boardColumnLabels = range(1, 7)
+ROW_LABELS = list('ABCDEF')
+COL_LABELS = list(map(str,range(1, 7)))
 
 
-class Block:
+class GamePiece:
 
-    def __init__(self, name, bId, blockColor,
-                 checkRotated, checkFlipped, blockArray):
+    def __init__(self, name, uid, color,
+                 checkRotated, checkFlipped, mask):
         self.name = name
-        self.bId = bId
-        self.blockArray = np.array(blockArray)
-        self.blockColor = blockColor
+        self.uid = uid
+        self.color = color
         self.checkRotated = checkRotated
         self.checkFlipped = checkFlipped
+        self.mask = np.array(mask)
 
 
 class Dice:
-    sides = []
-    face = 0
 
-    def __init__(self, faceList):
-        self.sides = faceList
-        self.roll()
+    def __init__(self, face_list):
+        self.faces = face_list
 
     def roll(self):
-        maxNum = len(self.sides) - 1
-        self.face = random.randint(0, maxNum)
-        return self.sides[self.face]
-
-    def getSpaceLocation(self):
-        dieFace = self.sides[self.face]
-        rowAlpha = dieFace[0]
-        col = int(dieFace[1]) - 1
-        row = boardRowLabels.index(rowAlpha)
-        return (row, col)
+        random_face_index = random.randrange(len(self.faces))
+        face = self.faces[random_face_index]
+        row = ROW_LABELS.index(face[0])
+        col = COL_LABELS.index(face[1])
+        return face, (row, col)
 
 
 class Board:
@@ -54,40 +46,44 @@ class Board:
 
     def draw(self):
 
-        boardData = np.ones((6, 6, 3))
+        data_with_color = np.ones((6, 6, 3))
         fig, ax = plt.subplots()
 
         for rowIndex, row in enumerate(self.space):
             for colIndex, col in enumerate(row):
-                if col == 99:
-                    circ = plt.Circle((colIndex, rowIndex), radius=0.45, color=(0.6, 0.4, 0.05))
+                if col == 99: # blocker piece
+                    color = all_pieces[0].color
+                    circ = plt.Circle((colIndex, rowIndex), radius=0.45, color=color)
                     ax.add_patch(circ)
                 elif col > 0:
-                    boardData[rowIndex, colIndex] = blockColors.get(col, [0, 0, 0])
+                    data_with_color[rowIndex, colIndex] = piece_colors.get(col, [0, 0, 0])
 
-        ax.imshow(boardData)
+        ax.imshow(data_with_color)
         ax.xaxis.tick_top()
         ax.set_xticks(np.arange(6))
         ax.set_yticks(np.arange(6))
 
-        ax.set_xticklabels(boardColumnLabels)
-        ax.set_yticklabels(boardRowLabels)
+        ax.set_xticklabels(COL_LABELS)
+        ax.set_yticklabels(ROW_LABELS)
 
         plt.draw()
         plt.pause(0.001)
         # show plot and allow processing to continue
 
     def drawToConsole(self):
-        output = "    1  2  3  4  5  6\n"
+        output = "    "
+        output += "  ".join(COL_LABELS) + "\n"
         for rowIndex, row in enumerate(self.space):
-            output += boardRowLabels[rowIndex] + " "
+            output += ROW_LABELS[rowIndex] + " "
             for colIndex, col in enumerate(row):
                 output += str(col).rjust(3)
             output += '\n'
         print(output)
 
     def isSolved(self):
-        return (self.space[self.space == 0].size == 0)
+        empty_spaces = self.space[self.space == 0]
+        solved = (empty_spaces.size == 0)
+        return solved
 
     def pieceFitsAtSpace(self, piece, row, col):
         flipList = [0,1] if piece.checkFlipped else [0]
@@ -95,13 +91,13 @@ class Board:
 
         for flip in flipList:
             for rotation in rotateList:
-                pieceRows, pieceCols = piece.blockArray.shape
+                pieceRows, pieceCols = piece.mask.shape
 
                 if rotation % 2 != 0: # for odd rotations, swap row & col sizes
                     pieceRows, pieceCols = pieceCols, pieceRows
 
                 boardSlice = self.space[row:row+pieceRows, col:col+pieceCols]
-                pieceSlice = np.fliplr(piece.blockArray) if flip else piece.blockArray
+                pieceSlice = np.fliplr(piece.mask) if flip else piece.mask
                 pieceSlice = np.rot90(pieceSlice, rotation)
                 if boardSlice.shape != pieceSlice.shape:
                     continue # this shape & orientation extends past the board edges, skip
@@ -113,21 +109,17 @@ class Board:
         return None # does not fit
 
     def placePiece(self, piece, row, col, orientation):
-        pieceRows, pieceCols = piece.blockArray.shape
+        pieceRows, pieceCols = piece.mask.shape
         if orientation[0] % 2 != 0: # for odd rotations, swap row & col sizes
             pieceRows, pieceCols = pieceCols, pieceRows
 
         boardSlice = self.space[row:row+pieceRows, col:col+pieceCols]
-        pieceSlice = np.fliplr(piece.blockArray) if orientation[1] else piece.blockArray
+        pieceSlice = np.fliplr(piece.mask) if orientation[1] else piece.mask
         pieceSlice = np.rot90(pieceSlice, orientation[0])
 
-        addSlice = pieceSlice * piece.bId
+        addSlice = pieceSlice * piece.uid
         boardSlice[:] += addSlice # we want to replace the range, not update reference to point to addSlice
 
-
-    def setSpace(self, loc, val):
-        row,col = loc[0],loc[1]
-        self.space[row][col] = val
         
 d = []
 d.append(Dice(['A1','F3','D1','E2','D2','C1']))
@@ -138,22 +130,25 @@ d.append(Dice(['A6','F1','A6','F1','A6','F1']))
 d.append(Dice(['B4','C3','C4','D3','E3','D4']))
 d.append(Dice(['D5','E4','E5','E6','F4','F5']))
 
-allPieces = []
-allPieces.append(Block('Blue', 1, [0,0,1.0], False, False, [[True]]))
-allPieces.append(Block('Brown', 2, [0.5,0.3,0.3], True, False, [[True, True]]))
-allPieces.append(Block('Orange', 3, [1.0,0.4,0], True, False, [[True, True, True]]))
-allPieces.append(Block('Grey', 4, [0.5,0.5,0.5], True, False, [[True, True, True, True]]))
-allPieces.append(Block('Red', 5, [1.0,0,0], True, True, [[False, True, True],
+all_pieces = []
+all_pieces.append(GamePiece('Blocker', 99, [0.6, 0.4, 0.05], False, False, [[True]]))
+all_pieces.append(GamePiece('Blue', 1, [0,0,1.0], False, False, [[True]]))
+all_pieces.append(GamePiece('Brown', 2, [0.5,0.3,0.3], True, False, [[True, True]]))
+all_pieces.append(GamePiece('Orange', 3, [1.0,0.4,0], True, False, [[True, True, True]]))
+all_pieces.append(GamePiece('Grey', 4, [0.5,0.5,0.5], True, False, [[True, True, True, True]]))
+all_pieces.append(GamePiece('Red', 5, [1.0,0,0], True, True, [[False, True, True],
                                                         [True, True, False]]))
-allPieces.append(Block('Yellow', 6, [1.0,0.7,0], True, False,[[True, True, True],
+all_pieces.append(GamePiece('Yellow', 6, [1.0,0.7,0], True, False,[[True, True, True],
                                                             [False, True, False]]))
-allPieces.append(Block('Cyan', 7, [0.2,0.5,1.0], True, True, [[True, True, True],
+all_pieces.append(GamePiece('Cyan', 7, [0.2,0.5,1.0], True, True, [[True, True, True],
                                                             [True, False, False]]))
-allPieces.append(Block('Green', 8, [0,1.0,0], False, False, [[True, True],
+all_pieces.append(GamePiece('Green', 8, [0,1.0,0], False, False, [[True, True],
                                                             [True, True]]))
-allPieces.append(Block('Purple', 9, [0.5,0,0.5], True, False, [[True, True],
+all_pieces.append(GamePiece('Purple', 9, [0.5,0,0.5], True, False, [[True, True],
                                                                 [True, False]]))
-blockColors = {x.bId: x.blockColor for x in allPieces}
+piece_colors = {x.uid: x.color for x in all_pieces}
+
+play_pieces = all_pieces[1:]  # all pieces except the Blocker are available to play
 
 strategicSort = [4, 5, 6, 7, 8, 9, 3, 2, 1]
 
@@ -161,9 +156,10 @@ theBoard = Board()
 
 print("Rolling dice...")
 diceResult = ' '
-for i in range(len(d)):
-    diceResult += d[i].roll() + ' '
-    theBoard.setSpace(d[i].getSpaceLocation(),99)
+for i in range(len(d)):  # iterate dice list and roll each one
+    face, (row, col) = d[i].roll()
+    diceResult += face + ' '
+    theBoard.placePiece(all_pieces[0], row, col, (0, 0))  # place the blocker pieces
 diceResult += '\n'
 print(diceResult)
 
@@ -198,12 +194,12 @@ def recursiveSolve(board, remaining, stopAtFirstSolution = False):
     return False # cannot solve at this depth
 
 
-allPieces.sort(key=lambda x: strategicSort.index(x.bId))
+play_pieces.sort(key=lambda x: strategicSort.index(x.uid))
 print('Using following stategy')
-print([x.name for x in allPieces])
+print([x.name for x in play_pieces])
 print('Solving...')
 start_time = time.process_time()
 nSolutions = 0
-recursiveSolve(theBoard, allPieces, stopAtFirstSolution = True)
+recursiveSolve(theBoard, play_pieces, stopAtFirstSolution = True)
 print('Found {} solutions in {:.2f} seconds'.format(nSolutions, time.process_time() - start_time))
 plt.show() #keeps program running until the plot is closed
